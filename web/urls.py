2d8e75a5dac26f122fe50bb5439b3ed34490eb33
lamiiz <lamiizmuhammed.l4miiix@gmail.com>
2023-05-26 00:20:47 +0530

    ebook

--- web/urls.py
+++ web/urls.py
@@ -7,11 +7,11 @@ app_name='web'
 
 urlpatterns = [
     path('', views.index,name='index'),
-    path('login', views.login_view,name='login'),
+    path('login/', views.login_view,name='login'),
     path('signup/', views.signup, name='signup_view'),
-    path('logout', views.logout_view, name='logout'),
-    path('about', views.about,name='about'),
-    path('contact', views.contact,name='contact'),
-    path('shop', views.shop,name='shop'),
+    path('logout/', views.logout_view, name='logout'),
+    path('about/', views.about,name='about'),
+    path('contact/', views.contact,name='contact'),
+    path('shop/', views.shop,name='shop'),
     path('single_page/<int:id>/', views.single_page, name='single_page'),
 ]
